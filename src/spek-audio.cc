3db4c6bbfa4a73eb706e8e04da95fb0598fe2986
withmorten <morten.with@gmail.com>
2017-12-06 19:19:44 +0100

    Revert change to bps behaviour

--- src/spek-audio.cc
+++ src/spek-audio.cc
@@ -158,7 +158,7 @@ std::unique_ptr<AudioFile> Audio::open(const std::string& file_name, int stream)
             // These decoders set both bps and bitrate.
             bits_per_sample = 0;
         }
-        if (codecpar->codec_id == AV_CODEC_ID_ALAC) {
+        if (bits_per_sample) {
             bit_rate = 0;
         }
         channels = codecpar->channels;
